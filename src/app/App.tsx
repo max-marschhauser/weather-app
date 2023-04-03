c2ce705e252a7934df3b61c321510af624bd60ee
Max Marschhauser <max.marschhauser@gmail.com>
2023-04-03 12:58:43 +0200

    added axios code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,46 @@
-import React, { useState } from "react";
-import getWeather from "./getWeather";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./app.scss";
 
 export default function App() {
-	const [items, setItems] = useState(getWeather);
+	let [time, setTime] = useState([]);
+	let [temperature, setTemperature] = useState([]);
+	const [loading, setLoading] = useState(true);
 
-	console.log(items);
+	useEffect(() => {
+		setLoading(true);
+		axios
+			.get(
+				`https://api.open-meteo.com/v1/forecast?latitude=45.55&longitude=18.69&hourly=temperature_2m&timezone=Europe%2FBerlin`
+			)
+			.then((response) => {
+				setLoading(false);
+				setTime(response.data.hourly.time);
+				setTemperature(response.data.hourly.temperature_2m);
+			})
+			.catch(() => {
+				alert("Error");
+			});
+	}, []);
 
 	return (
-		<>
-			<ul>
-				<li>Temperature:</li>
-				<li>Windspeed: </li>
-			</ul>
-		</>
+		<div>
+			{loading ? (
+				<div>Loading...</div>
+			) : (
+				<div className="list">
+					<ul>
+						{time.map((item, id) => {
+							return <li key={id}>{item}</li>;
+						})}
+					</ul>
+					<ul>
+						{temperature.map((item, id) => {
+							return <li key={id}>{item}°C</li>;
+						})}
+					</ul>
+				</div>
+			)}
+		</div>
 	);
 }
